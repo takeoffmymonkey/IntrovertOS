8167312dd5f28894084dae401b3de28f76d2cfbc
takeoff <galukhin@gmail.com>
2018-01-02 13:31:56 +0200

    add timeline menu

--- app/src/main/java/com/example/android/introvert/Fragments/TimelineFragment.java
+++ app/src/main/java/com/example/android/introvert/Fragments/TimelineFragment.java
@@ -3,15 +3,16 @@ package com.example.android.introvert.Fragments;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.android.introvert.Activities.MainActivity;
 import com.example.android.introvert.Activities.NoteActivity;
@@ -19,6 +20,7 @@ import com.example.android.introvert.IntrovertDbHelper;
 import com.example.android.introvert.R;
 import com.example.android.introvert.Utils;
 
+import static com.example.android.introvert.IntrovertDbHelper.NOTES_TABLE_NAME;
 import static com.example.android.introvert.IntrovertDbHelper.SETTINGS_TABLE_NAME;
 
 /**
@@ -34,6 +36,12 @@ public class TimelineFragment extends Fragment {
     private IntrovertDbHelper dbHelper;
 
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -45,22 +53,37 @@ public class TimelineFragment extends Fragment {
         View timelineView = inflater.inflate(R.layout.fragment_timeline, container,
                 false);
 
-        Button b = (Button) timelineView.findViewById(R.id.add_note);
-
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), NoteActivity.class);
-                startActivity(intent);
-                putInDb();
-                Utils.dumpTableExternal(db, SETTINGS_TABLE_NAME);
-
-            }
-        });
-
         //Return inflated layout for this fragment
         return timelineView;
 
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_timeline, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menu_timeline_dump_notes:
+                Utils.dumpTableExternal(db, NOTES_TABLE_NAME);
+                return true;
+
+            case R.id.menu_timeline_dump_settings:
+                Utils.dumpTableExternal(db, SETTINGS_TABLE_NAME);
+                return true;
+
+            case R.id.menu_timeline_add_note:
+                Intent intent = new Intent(getContext(), NoteActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
